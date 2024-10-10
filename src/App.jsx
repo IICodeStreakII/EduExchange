fe4683fe9a311ef7677cb2730c9df99c9c66c592
Vikram <vikramranjan71122@gmail.com>
2024-10-10 12:39:19 +0530

    first push

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import chai from "./new.jsx";
+
+const username = "Vikram"; // expression
+function App() {
+  return (
+    <>
+      <p1>I am {username} This Is My First Parahgraph</p1>
+      {/* evaluated  expression  can not write for loop or if element or any thing else */}
+      <chai />
+    </>
+  );
+}
+
+export default App;
---
+++ src/new.jsx
@@ -0,0 +1,5 @@
+function chai() {
+  return <h1>Hello</h1>;
+}
+
+export default chai;
